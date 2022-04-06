93c768a22efcbaf9fc70e1ee3f17ad729da40e64
Austin Caprio <austincaps98@gmail.com>
2022-04-05 22:20:56 -0600

    Wrote a capitalize method

---
+++ UtahCrashesCracked/Models/Capitalize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UtahCrashesCracked.Models
+{
+    public class Capitalize
+    {
+        string UppercaseFirst(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+    }
+
+}
