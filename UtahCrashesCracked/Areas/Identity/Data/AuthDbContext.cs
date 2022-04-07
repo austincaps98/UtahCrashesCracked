4c33e601ab2c1d0578e146dfbdb31a363f665aea
MelRobles <testMelEmail@test.com>
2022-04-07 15:24:32 -0600

    removed .data from identity not needed

--- UtahCrashesCracked/Areas/Identity/Data/AuthDbContext.cs
+++ UtahCrashesCracked/Areas/Identity/Data/AuthDbContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using UtahCrashesCracked.Areas.Identity.Data;
+using UtahCrashesCracked.Areas.Identity;
 
 namespace UtahCrashesCracked.Data
 {
