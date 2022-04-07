48f0480103118e0203130dbb508ec6e70dce2f16
Greg Marks <g123616@byu.edu>
2022-04-07 15:12:23 -0600

    Quick Updates

--- UtahCrashesCracked/Startup.cs
+++ UtahCrashesCracked/Startup.cs
@@ -37,7 +37,7 @@ namespace UtahCrashesCracked
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSingleton<InferenceSession>(
-                new InferenceSession("Models/crashdata4.onnx")
+                new InferenceSession("wwwroot/crashdata4.onnx")
                 );
         }
 
